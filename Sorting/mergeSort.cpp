dbad59e183b86cb279da5b598c4d741b169cd4b7
Adilet Absatov <adl.absatov@gmail.com>
2019-05-23 14:41:32 +0600

    Merge sort refactored

--- Sorting/mergeSort.cpp
+++ Sorting/mergeSort.cpp
@@ -1,35 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 5000;
-int a[N];
+const int N = 100100;
+int a[N], n;
  
-void Merge(int l, int m, int r) 
+void Merge(int l, int r) 
 {
-    int i, j, k, cur[r - l + 1];
-    i = l, k = 0, j = m + 1;
-    while (i <= m && j <= r) 
+    int cur[r - l + 1], mid = l + r >> 1;
+    int i = l, j = mid + 1, k = 0;
+    while (i <= mid && j <= r) 
     {
-        if (a[i] < a[j]) 
-            cur[k++] = a[i++];
-        else 
-            cur[k++] = a[j++];
+        if (a[i] < a[j]) cur[k++] = a[i++];
+        else cur[k++] = a[j++];
     }
-    while (i <= m) 
-        cur[k++] = a[i++];
-    while (j <= r) 
-        cur[k++] = a[j++];
+    while (i <= mid) cur[k++] = a[i++];
+    while (j <= r) cur[k++] = a[j++];
     for(int i = l; i <= r; i++) 
         a[i] = cur[i - l];
 }
  
 void MergeSort(int l, int r) 
 {
-    if(l < r) 
+    if (l < r) 
     {
-        int m = l + (r - l) / 2;
-        MergeSort(l, m);
-        MergeSort(m + 1, r);
-        Merge(l, m, r);
+        int mid = l + r >> 1;
+        MergeSort(l, mid);
+        MergeSort(mid + 1, r);
+        Merge(l, r);
     }
+}
+
+int main()
+{   cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    MergeSort(0, n - 1);
+    for (int i= 0; i < n; i++)
+        cout << a[i] << ' ';
 }
