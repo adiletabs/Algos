e356bcf51563beaa4ecab63b957a7d22339cf825
Adilet Absatov <adl.absatov@gmail.com>
2019-09-13 12:09:14 +0300

    Update z_function.cpp

--- Strings/z_function.cpp
+++ Strings/z_function.cpp
@@ -5,8 +5,7 @@ vector<int> z_function(string s)
 {
 	int n = (int)s.length();
 	vector<int> z (n);
-	for (int i = 1, l = 0, r = 0; i < n; i++) 
-	{
+	for (int i = 1, l = 0, r = 0; i < n; i++) {
 		if (i <= r)
 			z[i] = min(r - i + 1, z[i - l]);
 		while (i + z[i] < n && s[z[i]] == s[i + z[i]])
@@ -17,28 +16,24 @@ vector<int> z_function(string s)
 	return z;
 }
 
-// get the string from the given z-function
+// get the string from given z-function
 string get(vector<int> z)
 {
 	string ALPHA = "abcedfghijklmnopqrstuvwxyz", res = "";
 	int len = 0, ind = 0, j;
 	z[0] = 0;
-	for (int i = 0; i < z.size(); i++)
-	{
-		if (z[i] == 0 && len == 0)
-		{
+	for (int i = 0; i < z.size(); i++) {
+		if (z[i] == 0 && len == 0) {
 			if (ind < 52)
 				res += ALPHA[ind++];
 			else
 				res += ALPHA[ind - 1];
 		}
-		if (z[i] > len)
-		{
+		if (z[i] > len) {
 			len = z[i];
 			j = 0;
 		}
-		if (len > 0)
-		{
+		if (len) {
 			res += res[j];
 			j++;
 			len--;
