9c177777e4ed502d999a173ca3f2dcb66aed382b
Adilet Absatov <adl.absatov@gmail.com>
2019-09-14 16:57:15 +0300

    Update polygon_area.cpp

--- Geometry/polygon_area.cpp
+++ Geometry/polygon_area.cpp
@@ -9,8 +9,7 @@ struct point
 double area(vector<point> v)
 {
 	double res = 0;
-	for (int i = 0; i < v.size(); i++)
-	{
+	for (int i = 0; i < v.size(); i++) {
 		point p1 = i ? v[i - 1] : v.back(), p2 = v[i];
 		res += (p1.x - p2.x) * (p1.y + p2.y);
 	}
