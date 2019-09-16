ba545974eb6b7af903100ea8c62c51944cfa0d60
Adilet Absatov <adl.absatov@gmail.com>
2019-09-16 12:31:35 +0300

    Update euler.cpp

--- Math/euler.cpp
+++ Math/euler.cpp
@@ -41,10 +41,8 @@ void calc()
 {                       
     for (int i = 1; i < N; i++) 
         phi[i] = i; 
-    for (int p = 2; p < N; p++) 
-    { 
-        if (phi[p] == p) 
-        { 
+    for (int p = 2; p < N; p++) { 
+        if (phi[p] == p) { 
             phi[p] = p - 1; 
             for (int i = 2 * p; i < N; i += p) 
                 phi[i] = (phi[i] / p) * (p - 1); 
