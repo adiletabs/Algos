f4e9b4ee2d2242767fd5796263fdcb6dcecf69e9
Adilet Absatov <adl.absatov@gmail.com>
2019-09-20 11:28:29 +0300

    Update heap.cpp

--- DataStructures/STL/heap.cpp
+++ DataStructures/STL/heap.cpp
@@ -11,52 +11,39 @@ class HEAP
 		T h[N];
 
 	public:
-		void siftUp(int pos)
-		{
+		void siftUp(int pos) {
 			int npos = pos >> 1;
-			while (npos && h[pos] > h[npos])
-			{
+			while (npos && h[pos] > h[npos]) {
 				swap(h[pos], h[npos]);
 				pos = npos;
 				npos = pos >> 1;
 			}
 		}
 
-		void siftDown(int pos, T a[], int sz)
-		{
-			while (pos <= sz)
-			{
+		void siftDown(int pos, T a[], int sz) {
+			while (pos <= sz) {
 				int l = pos + pos;
 				int r = l + 1;
 				if (l > sz) break;
-				else if (r > sz)
-				{
+				else if (r > sz) {
 					if (a[l] > a[pos])
 						swap(a[l], a[pos]);
 					break;
 				}
 
 				if (a[l] <= a[pos] && a[r] <= a[pos]) break;
-				if (a[l] > a[pos] && a[r] > a[pos])
-				{
-					if (a[r] > a[l])
-					{
+				if (a[l] > a[pos] && a[r] > a[pos]) {
+					if (a[r] > a[l]) {
 						swap(a[pos], a[r]);
 						pos = r;
-					}
-					else
-					{
+					} else {
 						swap(a[pos], a[l]);
 						pos = l;
 					}
-				}
-				else if (a[l] > a[pos])
-				{
+				} else if (a[l] > a[pos]) {
 					swap(a[pos], a[l]);
 					pos = l;
-				}
-				else if (a[r] > a[pos])
-				{
+				} else if (a[r] > a[pos]) {
 					swap(a[pos], a[r]);
 					pos = r;
 				}
@@ -65,14 +52,12 @@ class HEAP
 
 		void siftDown(int pos) { siftDown(pos, h, size); }
 
-		void insert(T x)
-		{
+		void insert(T x) {
 			h[++size] = x;
 			siftUp(size);
 		}
 
-		void build(T a[], int n)
-		{
+		void build(T a[], int n) {
 			for (int i = 0; i < n; i++)
 				insert(a[i]);
 		}
@@ -81,15 +66,13 @@ class HEAP
 
 		int getSize() { return size; }
 
-		void heapSort(T a[], int n)
-		{
+		void heapSort(T a[], int n) {
 			build(a, n);
 			T help[n + 1];
 			int sz = n;
 			for (int i = 1; i <= n; i++)
 				help[i] = h[i];
-			for (int i = 0; i < n - 1; i++)
-			{
+			for (int i = 0; i < n - 1; i++) {
 				swap(help[1], help[n - i]);
 				siftDown(1, help, --sz);
 			}
@@ -97,34 +80,28 @@ class HEAP
 				a[i] = help[i + 1];
 		}
 
-		void delMax()
-		{
+		void delMax() {
 			h[1] = h[size--];
 			siftDown(1);
 		}
 
-		void increasePriority(int pos, T x)
-		{
+		void increasePriority(int pos, T x) {
 			if (pos > size) cout << "error\n";
-			else
-			{
+			else {
 				h[pos] = x;
 				siftUp(pos);
 			}
 		}
 
-		void decreasePriority(int pos, T x)
-		{
+		void decreasePriority(int pos, T x) {
 			if (pos > size) cout << "error\n";
-			else
-			{
+			else {
 				h[pos] = x;
 				siftDown(pos);
 			}
 		}
 
-		void print()
-		{
+		void print() {
 			for (int i = 1; i <= size; i++)
 				cout << h[i] << ' ';
 			cout << '\n';
