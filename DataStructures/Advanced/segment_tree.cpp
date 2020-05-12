3e4578bebe4a970d90483650b23d9c2fcbbc0164
Adilet Absatov <adl.absatov@gmail.com>
2020-05-13 05:00:16 +0600

    Updated segment_tree

--- DataStructures/Advanced/segment_tree.cpp
+++ DataStructures/Advanced/segment_tree.cpp
@@ -1,100 +1,109 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 100100;
-int a[N], n, q;
+struct Node {
+	int sum, min, add;
+};
 
-struct segment_tree
-{
-	int t[N << 2], to[N << 2];
-	segment_tree() {
-		memset(t, 0, sizeof t);
-		memset(t, 0, sizeof to);
-	}
-	void build(int v = 1, int tl = 1, int tr = n)
-	{
-		if (tl == tr)
-			t[v] = a[tl];
-		else {
-			int tm = tl + tr >> 1;
-			build(v + v, tl, tm);
-			build(v + v + 1, tm + 1, tr);
-			t[v] = t[v + v] + t[v + v + 1];
-		}
-	}
-	int get(int l, int r, int v = 1, int tl = 1, int tr = n)
-	{
-		if (l > r) return 0;             // INT_MAX for ST_min, -1 for ST_max
-		if (tl == l && tr == r) return t[v];
-		t[v + v] += to[v];
-		t[v + v + 1] += to[v];
-		push(v);
+const int N = 100100, inf = 2e9;
+int a[N], n, q;
+Node t[N << 2];
+
+void build(int v = 1, int tl = 1, int tr = n) {
+	if (tl == tr)
+		t[v].sum = t[v].min = a[tl];
+	else {
 		int tm = tl + tr >> 1;
-		return get(l, min(tm, r), v + v, tl, tm) + get(max(tm + 1, l), r, v + v + 1, tm + 1, tr);
+		build(v + v, tl, tm);
+		build(v + v + 1, tm + 1, tr);
+		t[v].sum = t[v + v].sum + t[v + v + 1].sum;
+		t[v].min = min(t[v + v].min, t[v + v + 1].min);
 	}
-	void upd(int pos, int x, int v = 1, int tl = 1, int tr = n)
-	{
-		if (tl == tr) {
-			t[v] = x;
-			a[pos] = x;
-		} else {
-			int tm = tl + tr >> 1;
-			if (pos <= tm) 
-				upd(pos, x, v + v, tl, tm);
-			else 
-				upd(pos, x, v + v + 1, tm + 1, tr);
-			t[v] = t[v + v] + t[v + v + 1];
-		}
-	}
-	void push(int v)
-	{
-		to[v + v] += to[v];
-		to[v + v + 1] += to[v];
-		to[v] = 0;
-	}
-	void add(int l, int r, int val, int v = 1, int tl = 1, int tr = n)
-	{
-		if (l > r) return;
-		if (tl == l && tr == r) {
-			t[v] += val;
-			to[v] += val;
-			return;
-		}
-		t[v + v] += to[v];
-		t[v + v + 1] += to[v];
-		push(v);
+}
+
+void update(int pos, int val, int v = 1, int tl = 1, int tr = n) {
+	if (tl == tr) {
+		t[v].sum = t[v].min = val;
+		a[pos] = val;
+	} else {
 		int tm = tl + tr >> 1;
-		add(l, min(r, tm), val, v + v, tl, tm);
-		add(max(tm + 1, l), r, val, v + v + 1, tm + 1, tr);
-		t[v] = t[v + v] + t[v + v + 1];
+		if (pos <= tm) 
+			update(pos, val, v + v, tl, tm);
+		else 
+			update(pos, val, v + v + 1, tm + 1, tr);
+		t[v].sum = t[v + v].sum + t[v + v + 1].sum;
+		t[v].min = min(t[v + v].min, t[v + v + 1].min);
 	}
-} T;
+}
+
+void push(int v) {
+	t[v + v].sum += t[v].add;
+	t[v + v + 1].sum += t[v].add;
+	t[v + v].add += t[v].add;
+	t[v + v + 1].add += t[v].add;
+	t[v].add = 0;
+}
+
+int getSum(int l, int r, int v = 1, int tl = 1, int tr = n) {
+	if (l > r) return 0;            
+	if (tl == l && tr == r) return t[v].sum;
+	push(v);
+	int tm = tl + tr >> 1;
+	return getSum(l, min(tm, r), v + v, tl, tm) + getSum(max(tm + 1, l), r, v + v + 1, tm + 1, tr);
+}
+
+int getMin(int l, int r, int v = 1, int tl = 1, int tr = n) {
+	if (l > r) return inf;            
+	if (tl == l && tr == r) return t[v].min;
+	push(v);
+	int tm = tl + tr >> 1;
+	return min(getMin(l, min(tm, r), v + v, tl, tm), getMin(max(tm + 1, l), r, v + v + 1, tm + 1, tr));
+}
+
+void add(int l, int r, int val, int v = 1, int tl = 1, int tr = n) {
+	if (l > r) return;
+	if (tl == l && tr == r) {
+		t[v].sum += val;
+		t[v].add += val;
+		return;
+	}
+	push(v);
+	int tm = tl + tr >> 1;
+	add(l, min(r, tm), val, v + v, tl, tm);
+	add(max(tm + 1, l), r, val, v + v + 1, tm + 1, tr);
+	t[v].sum = t[v + v].sum + t[v + v + 1].sum;
+	t[v].min = min(t[v + v].min, t[v + v + 1].min);
+}
 
 int main()
 {
 	cin >> n;
 	for (int i = 1; i <= n; i++)
 		cin >> a[i];
-	T.build();
+	build();
 	cin >> q;
 	while (q--) {
-		string com;
-		cin >> com;
-		if (com == "get") {
+		string command;
+		cin >> command;
+		if (command == "sum") {
 			int l, r;
 			cin >> l >> r;
-			cout << T.get(l, r) << '\n';
-		} else if (com == "upd") {
-			int pos, x;
-			cin >> pos >> x;
-			T.upd(pos, x);
-		} else if (com == "add") {
-			int l, r, x;
-			cin >> l >> r >> x;
-			T.add(l, r, x);
+			cout << getSum(l, r) << '\n';
+		} else if (command == "min") {
+			int l, r;
+			cin >> l >> r;
+			cout << getMin(l, r) << '\n';
+		} else if (command == "update") {
+			int pos, val;
+			cin >> pos >> val;
+			update(pos, val);
+		} else if (command == "add") {
+			int l, r, val;
+			cin >> l >> r >> val;
+			add(l, r, val);
 		}
 		for (int i = 1; i <= n; i++)
-			cout << a[i] << ' ';
-		cout << endl;
+			cout << getSum(i, i) << ' ';
+		cout << '\n';
 	}
 }
